c56e6501c1f364a421e4c8be83604b9233992066
Ryan Osten <ostenrj@gmail.com>
2017-03-25 16:21:48 -0400

    get requests complete

--- scraper.js
+++ scraper.js
@@ -17,38 +17,54 @@ if(!fs.existsSync('data')){
   console.log('data folder already exists');
 }
 
-//create an array to store all of the shirt objects
-var shirtsObj = [];
-//b. should visit shirts4mike.com and uses /shirt.php as entry point to scrape information for 8 tee-shirts
-//Do not use hard coded URL like /shirts.php?id=101
-request('http://shirts4mike.com/shirt.php', function(error, response, html){
-  if(!error && response.statusCode == 200){
-    const $ = cheerio.load(html, {
+const req = http.get('http://www.shirts4mike.com/shirts.php', response => {
+  console.log(response.statusCode);
+  const shirtsObj = [];
+
+  let body = "";
+  //read the data
+  response.on('data', data => {
+    body += data.toString();
+  });
+
+  response.on('end', () => {
+    const $ = cheerio.load(body, {
       ignoreWhitespace: true
     });
-    const shirtObj = [];
-    //loop through unrdered list with class ".products", get list items and get <a href> value for each
+
     $('.products li a').each(function(){
-      //grab the list item href and make request to this path
-      const requestURL = `http://shirts4mike.com/${$(this).attr('href')}`
-      request(requestURL, function(error, response, html){
-        const $ = cheerio.load(html, {
-          ignoreWhitespace: true
+        console.log($(this).attr('href'));
+
+        //grab the list item href and make request to this path
+        const requestURL = `http://www.shirts4mike.com/${$(this).attr('href')}`;
+        const req = http.get(requestURL, response => {
+          console.log(response.statusCode);
+
+          let body = "";
+          //read data
+          response.on('data', data => {
+            body += data.toString();
+          });
+
+          response.on('end', () => {
+            const $ = cheerio.load(body, {
+              ignoreWhitespace: true
+            });
+
+            const title = $('.shirt-picture span img').attr('alt');
+            const price = $('span.price').text();
+            const imageURL = `http://shirts4mike.com/${$('.shirt-picture span img').attr('src')}`;
+
+            const shirt = {
+              "title": title,
+              "price": price,
+              "image": imageURL,
+              "URL": requestURL
+            }
+            shirtsObj.push(shirt);
+            console.log(shirtsObj);
+          });
         });
-
-        const title = $('.shirt-picture span img').attr('alt');
-        const price = $('span.price').text();
-        const imageURL = `http://shirts4mike.com/${$('.shirt-picture span img').attr('src')}`;
-
-        const shirt = {
-          "title": title,
-          "price": price,
-          "image": imageURL,
-          "URL": requestURL
-        }
-        shirtsObj.push(shirt);
-      })
-    })
-    console.log(shirtsObj);
-  }
-})
+    });
+  });
+});
