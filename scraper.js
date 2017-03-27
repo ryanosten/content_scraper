04434c06fa02c82ad397b922d28496c04bf4c4a1
Ryan Osten <ostenrj@gmail.com>
2017-03-27 19:43:09 -0400

    refactored with Require

--- scraper.js
+++ scraper.js
@@ -2,6 +2,76 @@
 
 const fs = require('fs');
 const cheerio = require('cheerio');
+const request = require('request');
+const json2csv = require('json2csv');
+
+const shirtsObj = [];
+
+function getShirts(anchor){
+  //grab the list item href and make request to this path
+  const url = `http://www.shirts4mike.com/${anchor.attr('href')}`;
+
+  request(url, (err, res, body) => {
+    if (err) {
+      console.error(new Error(err));
+      process.exit(1);
+    }
+
+    if (res.statusCode !== 200) {
+      console.error(new Error(`Received code: ${res.statusCode}`));
+      process.exit(2);
+    }
+
+    const $ = cheerio.load(body, {
+      ignoreWhitespace: true
+    });
+
+    const title = $('.shirt-picture span img').attr('alt');
+    const price = $('span.price').text();
+    const image = `http://shirts4mike.com/${$('.shirt-picture span img').attr('src')}`;
+
+    shirtsObj.push({
+      title,
+      price,
+      image,
+      URL: url
+    });
+    //console.log(shirtsObj)
+  });
+};
+
+if(!fs.existsSync('./data')){
+  fs.mkdirSync('./data');
+}
+
+request('http://www.shirts4mike.com/shirts.php', (err, res, body) => {
+  if (err) {
+    console.error(new Error(err));
+    process.exit(1);
+  }
+
+  if (res.statusCode !== 200) {
+    console.error(new Error(`Received code: ${res.statusCode}`));
+    process.exit(2);
+  }
+
+  const $ = cheerio.load(body, {
+    ignoreWhitespace: true
+  });
+
+  //const anchors = $('.products li a')
+
+  $('.products li a').each(function(){
+    //console.log($(this).attr('href'));
+    getShirts($(this));
+  });
+  console.log(shirtsObj);
+});
+
+/*'use strict';
+
+const fs = require('fs');
+const cheerio = require('cheerio');
 const http = require('http');
 const request = require('request');
 const json2csv = require('json2csv');
@@ -43,11 +113,6 @@ function getShirts(anchor){
   });
 };
 
-/*
-1. create scraper.js with command line app
-  a.check for a folder called 'data', if not exist then create one, if exists do nothing
-  */
-
 if(!fs.existsSync('data')){
   fs.mkdirSync('data');
 } else {
@@ -75,3 +140,4 @@ const req = http.get('http://www.shirts4mike.com/shirts.php', response => {
     });
   });
 });
+*/
