783c16d45580d3a09056ba04023480fb6224df63
Ryan Osten <ostenrj@gmail.com>
2017-03-27 21:29:51 -0400

    working with async

--- scraper.js
+++ scraper.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const request = require('request');
 const json2csv = require('json2csv');
+const async = require('async');
 
 const shirtsObj = [];
 
-function getShirts(anchor){
+const getShirts = function(item){
   //grab the list item href and make request to this path
-  const url = `http://www.shirts4mike.com/${anchor.attr('href')}`;
+  const url = `http://www.shirts4mike.com/${item.attribs.href}`;
 
   request(url, (err, res, body) => {
     if (err) {
@@ -36,7 +37,6 @@ function getShirts(anchor){
       image,
       URL: url
     });
-    //console.log(shirtsObj)
   });
 };
 
@@ -59,85 +59,15 @@ request('http://www.shirts4mike.com/shirts.php', (err, res, body) => {
     ignoreWhitespace: true
   });
 
-  //const anchors = $('.products li a')
+  const shirtAnchors = $('.products li a');
 
-  $('.products li a').each(function(){
-    //console.log($(this).attr('href'));
-    getShirts($(this));
-  });
-  console.log(shirtsObj);
-});
-
-/*'use strict';
-
-const fs = require('fs');
-const cheerio = require('cheerio');
-const http = require('http');
-const request = require('request');
-const json2csv = require('json2csv');
-
-const shirtsObj = [];
-
-function getShirts(anchor){
-  //grab the list item href and make request to this path
-  const requestURL = `http://www.shirts4mike.com/${anchor.attr('href')}`;
-
-  const req = http.get(requestURL, response => {
-    console.log(response.statusCode);
-
-    let body = "";
-    //read data
-    response.on('data', data => {
-      body += data.toString();
-    });
-
-    response.on('end', () => {
-      console.log('shirt page response end');
-      const $ = cheerio.load(body, {
-        ignoreWhitespace: true
-      });
-
-      const title = $('.shirt-picture span img').attr('alt');
-      const price = $('span.price').text();
-      const imageURL = `http://shirts4mike.com/${$('.shirt-picture span img').attr('src')}`;
-
-      const shirt = {
-        "title": title,
-        "price": price,
-        "image": imageURL,
-        "URL": requestURL
+  async.each(shirtAnchors, function(item, callback){
+    getShirts(item);
+  }, function(err){
+      if(err){
+      console.log('theres an error');
+      } else {
+        console.log('theres no error');
       }
-      shirtsObj.push(shirt);
-      console.log(shirtsObj);
-    });
-  });
-};
-
-if(!fs.existsSync('data')){
-  fs.mkdirSync('data');
-} else {
-  console.log('data folder already exists');
-}
-
-const req = http.get('http://www.shirts4mike.com/shirts.php', response => {
-  console.log(response.statusCode);
-
-  let body = "";
-  //read the data
-  response.on('data', data => {
-    body += data.toString();
-  });
-
-  response.on('end', () => {
-    console.log('shirts.php response end');
-    const $ = cheerio.load(body, {
-      ignoreWhitespace: true
-  });
-
-    $('.products li a').each(function(){
-      console.log($(this).attr('href'));
-      getShirts($(this));
-    });
   });
 });
-*/
