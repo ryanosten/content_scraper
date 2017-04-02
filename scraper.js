3a4d286c746c39f783a62e5c7c5fc7e788e672b6
Ryan Osten <ostenrj@gmail.com>
2017-04-02 09:53:44 -0400

    cleanups

--- scraper.js
+++ scraper.js
@@ -13,37 +13,37 @@ const getShirts = function(item, callback){
 
   //make get request to shirt pages
   request(url, (err, res, body) => {
-
+    //handle error
     if (err) {
       logError(err, `Couldn't connect to server`);
       return;
     }
-
+    //handle bad response
     if (res.statusCode !== 200) {
       logError(res.statusCode, res.statusMessage);
       return;
     }
+    //load parse body of response with cheerio
+    const $ = cheerio.load(body, {
+      ignoreWhitespace: true
+    });
 
-      const $ = cheerio.load(body, {
-        ignoreWhitespace: true
-      });
+    //grab title, price, imageURL from page
+    const Title = $('.shirt-picture span img').attr('alt');
+    const Price = $('span.price').text();
+    const ImageUrl = `http://shirts4mike.com/${$('.shirt-picture span img').attr('src')}`;
+    //get date/time
+    const date = new Date();
+    const Time = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
-      //grab title, price, imageURL from page
-      const Title = $('.shirt-picture span img').attr('alt');
-      const Price = $('span.price').text();
-      const ImageUrl = `http://shirts4mike.com/${$('.shirt-picture span img').attr('src')}`;
-      //get date/time
-      const date = new Date();
-      const Time = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-
-      //create object with shirt properties
-      callback(null, {
-        Title,
-        Price,
-        ImageUrl,
-        URL: url,
-        Time
-      });
+    //create object with shirt properties
+    callback(null, {
+      Title,
+      Price,
+      ImageUrl,
+      URL: url,
+      Time
+    });
   });
 };
 
@@ -69,37 +69,37 @@ const csvMagic = function(shirtData){
 //function for logging errors to console and logs/scraper-error.log
 const logError = function(error, message){
   const time = new Date();
-  console.error(`There's been an error. ${message} ${error}.`)
+  console.error(`There's been an error. ${message} ${error}.`);
   fs.writeFile('logs/scraper-error.log', `${time.toUTCString()} | ${error} ${message}\n`, {flag: "a"}, (err) => {
     if (err){
-      console.error(`There's been an error logging the file. ${err}`)
+      console.error(`There's been an error logging the file. ${err}`);
     }
   });
 }
 
 //make get request to http://www.shirts4mike.com/shirts.php entry point
 request('http://www.shirts4mike.com/shirts.php', (err, res, body) => {
+  //handle error
+  if(err) {
+    logError(err, `Couldn't connect to server`);
+    return;
+  }
+  //handle bad response
+  if(res.statusCode !== 200) {
+    logError(res.statusCode, res.statusMessage);
+    return;
+  }
+  //load cheerio for scraping html
+  const $ = cheerio.load(body, {
+    ignoreWhitespace: true
+  });
 
-    if(err) {
-      logError(err, `Couldn't connect to server`);
-      return;
-    }
+  //store anchor elements
+  const shirtAnchors = $('.products li a');
 
-    if(res.statusCode !== 200) {
-      logError(res.statusCode, res.statusMessage);
-      return;
-    }
-    //load cheerio for scraping html
-    const $ = cheerio.load(body, {
-      ignoreWhitespace: true
-    });
-
-    //store anchor elements
-    const shirtAnchors = $('.products li a');
-
-    //iterator over all shirt anchors, run getShirts on each anchor element, return results
-    async.map(shirtAnchors, getShirts, function(err, results){
-      //generate CSV
-      csvMagic(results);
-    });
+  //iterator over all shirt anchors, run getShirts on each anchor element, return results
+  async.map(shirtAnchors, getShirts, function(err, results){
+    //generate CSV
+    csvMagic(results);
+  });
 });
